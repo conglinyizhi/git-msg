c273155c66dd0dfd06552403db4b972fb8687982
conglinyizhi <cslbyivi@outlook.com>
2024-12-12 23:33:12 +0800

    ✨ feat(main): 添加新的 Go 项目，包含 .env 文件处理和 HTTP 请求功能

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+const LLM_API = "https://open.bigmodel.cn/api/paas/v4/chat/completions"
+
+type LLMBody struct {
+	Model    string `json:"model"`
+	Messages []msg  `json:"messages"`
+	Type     string `json:"type"`
+}
+
+type msg struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Fprintln(os.Stdout, []any{"Load .env file failed,%v", err}...)
+		return
+	}
+	token := os.Getenv("BIGMODEL_TOKEN")
+
+	req, err := http.NewRequest("POST", LLM_API, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, []any{"NewRequest failed,%v", err}...)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+token)
+	talkListMap := []map[string]string{
+		{
+			"role":    "system",
+			"content": "你好",
+		},
+		{
+			"role":    "user",
+			"content": "你好，我是chatglm-4-flash",
+		},
+	}
+	jsonObjectMap := map[string]interface{}{
+		"model":    "glm-4-flash",
+		"messages": talkListMap,
+		"type":     "text",
+	}
+	jsonObject, err := json.Marshal(jsonObjectMap)
+	fmt.Println(string(jsonObject))
+	if err != nil {
+		fmt.Fprintln(os.Stdout, []any{"json.Marshal failed,%v", err}...)
+		return
+	}
+
+	// 根据字符串准备一个Reader
+	bytesReader := bytes.NewReader(jsonObject)
+	bytesReaderCloser := io.NopCloser(bytesReader)
+
+	req.Body = bytesReaderCloser
+	req.ContentLength = int64(len(jsonObject))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, []any{"HTTP request failed,%v", err}...)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, []any{"Read response failed,%v", err}...)
+		return
+	}
+
+	fmt.Println(string(body))
+}
