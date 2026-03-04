6c91c832572547c922f269fc1c22d5d2f0c9c082
conglinyizhi <conglinyizhi@qq.com>
2026-03-05 00:59:41 +0800

    refactor(flag): 使用Var系列函数替代String/Bool返回值

--- flag.go
+++ flag.go
@@ -4,8 +4,8 @@ import "github.com/spf13/pflag"
 
 func parseCommandLineExData() CommandlineConfig {
 	cmdConfig := CommandlineConfig{}
-	cmdConfig.git = *pflag.StringP("git", "g", "git", "Git 指令替换，比如某些情况下用于替换为 yadm 等 Git Like 项目")
-	cmdConfig.init = *pflag.Bool("init", false, "尝试对当前版本的 git-msg 所需环境进行最大的初始化")
+	pflag.StringVarP(&cmdConfig.git, "git", "g", "git", "Git 指令替换，比如某些情况下用于替换为 yadm 等 Git Like 项目")
+	pflag.BoolVar(&cmdConfig.init, "init", false, "尝试对当前版本的 git-msg 所需环境进行最大的初始化")
 	pflag.Parse()
 	return cmdConfig
 }
