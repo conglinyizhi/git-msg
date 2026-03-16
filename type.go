732aba8e8458495061e9e149342ef7ef12761a63
conglinyizhi <conglinyizhi@qq.com>
2026-03-16 20:14:59 +0800

    refactor(type): 移除未使用的Event结构体

--- type.go
+++ type.go
@@ -1,20 +1,5 @@
 package main
 
-// 定义结构体来解析 JSON 数据
-type Event struct {
-	ID         string `json:"id"`
-	Created    int64  `json:"created"`
-	MODEL_NAME string `json:"model"`
-	Choices    []struct {
-		Index        int    `json:"index"`
-		FinishReason string `json:"finish_reason"`
-		Delta        struct {
-			Role    string `json:"role"`
-			Content string `json:"content"`
-		} `json:"delta"`
-	} `json:"choices"`
-}
-
 type RemoteAPIConfig struct {
 	API_KEY    string
 	BASE_URL   string
